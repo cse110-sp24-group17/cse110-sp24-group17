58cc089d8cd1eb2bc1bcbda5ca5d87b8c4210d00
P7chang <p7chang@ucsd.edu>
2024-05-12 18:14:30 -0700

    dummy functions

--- functions.js
+++ functions.js
@@ -9,3 +9,26 @@
 export function dummy(a, b) {
     return a + b;
 }
+/**
+ * 
+ * Dummy function that substracts b from a, with a redundant variable
+ * 
+ * @param {int} a first number
+ * @param {int} b second number
+ * @returns {int} Result of subtractions
+ */
+function dummy2(a, b) {
+    redundant = a + b;
+    return a - b;
+}
+
+/**
+ * 
+ * Another dummy function, should invoke Codacy warning since == is used instead of ===.
+ * 
+ * @param {int} input a number input
+ * @returns {boolean} returns whether input is 1
+ */
+function isOne(input) {
+    return input == 1;
+}
