cdd040c88b0a0fc868bb838f8e573b6ba766f536
P7chang <p7chang@ucsd.edu>
2024-05-12 18:00:16 -0700

    unit test example

---
+++ __tests__/unit.test.js
@@ -0,0 +1,14 @@
+import {
+    dummy
+  } from '../functions.js';
+  
+  // A dummy test to import a function from functions.js and testing it
+  test('1+3=4', () => {
+    expect(dummy(1, 3)).toBe(4);
+  });
+  test('3+1=4', () => {
+    expect(dummy(3, 1)).toBe(4);
+  });
+  test('3+0=3', () => {
+    expect(dummy(3, 0)).toBe(3);
+  });
