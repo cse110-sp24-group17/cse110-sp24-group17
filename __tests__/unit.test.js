a5fdd0c5311aa2d344e6fc4c5034ec90ad676189
P7chang <p7chang@ucsd.edu>
2024-06-03 21:35:48 -0700

    reverted failed tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -4,7 +4,7 @@ import {
   
   // A dummy test to import a function from functions.js and testing it
   test('1+3=4', () => {
-    expect(dummy(1, 3)).toBe(5);
+    expect(dummy(1, 3)).toBe(4);
   });
   test('3+1=4', () => {
     expect(dummy(3, 1)).toBe(4);
