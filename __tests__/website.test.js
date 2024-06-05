81980df6e0b92ccf9d1262b621913fe6f841f5c1
Melissa2002 <mosherof@ucsd.edu>
2024-06-05 16:56:48 -0700

    Fixed 3/5 E2E tests
    Checking original elements in folder, adding folder, and adding file work. Need to implement delete file and switch views test

--- __tests__/website.test.js
+++ __tests__/website.test.js
@@ -1,98 +1,102 @@
 describe('Basic user flow for Website', () => {
     beforeAll(async () => {
-        
-        
         await page.goto('https://cse110-sp24-group17.github.io/cse110-sp24-group17/source/main');
     });
   
-    // Check to make sure that the file starts off with an empty directory
+    // Check to make sure that the file starts off with a root directory
     it('Initial File Explorer - it is empty ', async () => {
-      console.log('Checking files...');
-      // Query select to make sure the directory is present
-      const children= await page.$eval('.directoryContainer', el => el.children);
-      // Expect there there to be a project view 
-      expect(Object.keys(children).length).toBe(0);
+        console.log('Checking files...');
+        // Query select the shadow root of the file explorer
+        const fileExplorer = await page.$('file-explorer');
+        const shadow = await fileExplorer.getProperty("shadowRoot");
+        // Get the root directory
+        const root = await shadow.$(".root");
+
+      // Check if the root exists
+      expect(root).not.toBeNull();
     }, 50000);
 
-    // Check to make sure that a file is added correctly
+    // Check to make sure that a file can be added
     it('Adding to File Explorer', async () => {
         console.log('Adding files...');
 
-        // Query the button to add a file and click on it
+        // Query the addFile button
         const addFileButton = await page.$('#addFile');
         await addFileButton.click();
-        const form = await page.$("#new-file-name");
 
-        try{
-            // Fill in name for the new file and submit it
-            await form.click();
-            await page.keyboard.type("ThisIsANewFile");
-            const submit = await page.$('input[type=submit]')
-            await submit.click();
+        // Get the shadow root of the file explorer
+        const fileExplorer = await page.$('file-explorer');
+        const shadow = await fileExplorer.getProperty("shadowRoot");
 
-            // Check that a new file was added
-            const children= await page.$eval('.directoryContainer', el => el.children);
-            expect(Object.keys(children).length).toBe(1);
-        } 
-        catch (err){
-            console.error(err);
-            console.log("The form to submit a name currently is not implemented");
-            expect(0).toBe(1);
-        }
+        // Record children files and folders from the beginning
+        const ogChildren = await shadow.$eval("#file", el => el.children);
+
+        // Submit the new file name 
+        const form = await shadow.$("#add-file");
+        await form.click();
+        await page.keyboard.type("ThisIsANewFile");
+        const submit = await form.$('input[type=submit]')
+        await submit.click();
+
+        // Check that one element was added
+        const childrenFiles = await shadow.$eval("#file", el => el.children);
+        expect(Object.keys(childrenFiles).length).toBe(Object.keys(ogChildren).length + 1);
       }, 50000);
-
+    
     // Check to make sure that a folder is added correctly
     it('Adding to Folder Explorer', async () => {
         console.log('Adding folder...');
 
-        const ogFileNum = await page.$eval('.directoryContainer', el => el.children.length);
+        // Query the addFolder button
+        const addFolderButton = await page.$('#addFolder');
+        await addFolderButton.click();
 
-        // Query the button to add a folder
-        const addFileButton = await page.$('#addFolder');
-        await addFileButton.click();
+        // Get the shadow root of the file explorer
+        const fileExplorer = await page.$('file-explorer');
+        const shadow = await fileExplorer.getProperty("shadowRoot");
 
-        // Fill in the new name of the folder and submit it 
-        const form = await page.$("#new-folder-name");
-        try{
-            await form.click();
-            await page.keyboard.type("ThisIsANewFolder");
-            const submit = await page.$('input[type=submit]')
-            await submit.click();
-            const children= await page.$eval('.directoryContainer', el => el.children);
+        // Record children files and folders from the beginning
+        const ogChildren = await shadow.$eval("#file", el => el.children);
 
-            // Check that the folder was added
-            expect(Object.keys(children).length).toBe(ogFileNum + 1);
-        } 
-        catch (err){
-            console.error(err);
-            console.log("The form to submit a name currently is not implemented");
-            expect(0).toBe(1);
-        }
+        // Submit the new folder name 
+        const form = await shadow.$("#add-folder");
+        await form.click();
+        await page.keyboard.type("ThisIsANewFolder");
+        const submit = await form.$('input[type=submit]')
+        await submit.click();
+
+        // Check that one element was added
+        const childrenFiles = await shadow.$eval("#file", el => el.children);
+        expect(Object.keys(childrenFiles).length).toBe(Object.keys(ogChildren).length + 1);
       }, 50000);
 
-      // Check to make sure that a element in the directory can be deleted
+    // FIXME
+    // Check to make sure that a element in the directory can be deleted
     it('Deleting from File Explorer', async () => {
         console.log('Deleting an element from file explorer...');
 
-        const ogFiles = await page.$eval('.directoryContainer', el => el.children);
-        const ogFilesLength = ogFiles.length;
+        // Get the shadow root of the file explorer
+        const fileExplorer = await page.$('file-explorer');
+        const shadow = await fileExplorer.getProperty("shadowRoot");
+
+        // Record children files and folders from the beginning
+        let children = await shadow.$("#file");
+        const numChildren = Object.keys(children).length;
 
         // Query the button to delete elements
-        const addFileButton = await page.$('#trashIcon');
-        await addFileButton.click();
+        const deleteButton = await page.$('#trashIcon');
+        await deleteButton.click();
 
-        // Fill in the new name of the folder and submit it 
-        try{
-            await ogFiles[0].click();
-            expect(ogFiles.length).toBe(ogFilesLength - 1);
-        } 
-        catch (err){
-            console.error(err);
-            console.log("No elements can be added atm")
-            expect(0).toBe(1);
-        }
+        // Get the first text file and delete it  
+        const childToDelete =await shadow.$(".file-entry");
+        await childToDelete.click();
+    
+        let newChildren = await shadow.$("#file");
+        expect(numChildren - 1).toBe(Object.keys(newChildren).length);
+
       }, 50000);
 
+    // FIXME
     // Switch from file explorer view to project view
     it('Switching from file explorer view to project view', async () => {
         console.log('Switching view...');
@@ -117,6 +121,6 @@ describe('Basic user flow for Website', () => {
         
         const file = await page.$eval('#ProjectView', el => el.getAttribute("class"));
         expect(file).toBe(journalOffset);
-      }, 50000);
+      }, 50000); 
   
   });
