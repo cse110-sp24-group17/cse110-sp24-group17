0b15c424af68af08874bfd9803c4d64f4820c735
Jacob Roner <jroner@ucsd.edu>
2024-06-04 21:33:59 -0700

    Started implementing functionality from file explorer to main.html.

--- source/swipeAnimation.js
+++ source/swipeAnimation.js
@@ -6,3 +6,13 @@ var swiper = new Swiper('.Slider-container', {
   longSwipes: true,
   speed: 700,
 });
+
+window.addEventListener('load', function () {
+  const ele = document.getElementById('file-explorer');
+  ele.onFileMouseEnter = () => {
+    swiper.disable();
+  };
+  ele.onFileMouseLeave = () => {
+    swiper.enable();
+  };
+});
