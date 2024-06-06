7147e44b0f5afe70070f9dfe83f5d9b450fba1e4
Ruiping <irisfang123@gmail.com>
2024-06-05 17:57:30 -0700

    add the textare input event, when user finishing typing, the content will store into the scratch.txt in the projectViews

--- source/swipeAnimation.js
+++ source/swipeAnimation.js
@@ -1,7 +1,7 @@
-import App from './modules/models/app.js';
+import App from "./modules/models/app.js";
 
-var swiper = new Swiper('.Slider-container', {
-  effect: 'cards',
+var swiper = new Swiper(".Slider-container", {
+  effect: "cards",
   grabCursor: true,
   centerdSlides: true,
   loop: true,
@@ -9,8 +9,8 @@ var swiper = new Swiper('.Slider-container', {
   speed: 700,
 });
 
-window.addEventListener('load', function () {
-  const ele = document.getElementById('file-explorer');
+window.addEventListener("load", function () {
+  const ele = document.getElementById("file-explorer");
   ele.onFileMouseEnter = () => {
     swiper.disable();
   };
@@ -18,32 +18,49 @@ window.addEventListener('load', function () {
     swiper.enable();
   };
 
-  const markdown_editor = document.getElementById('markdown-editor');
+  const markdown_editor = document.getElementById("markdown-editor");
   ele.onFileOpen = (file) => {
     markdown_editor.file = file;
   };
 
-  const calendar = document.getElementById('calendar');
+  const calendar = document.getElementById("calendar");
   calendar.onDatePicked = (date) => {
     if (!App.get_file_store().get_file("journals")) {
       App.get_file_store().create_directory("journals");
     }
-    let file = App.get_file_store().get_file("journals/"+date.toDateString() + '.md');
+    let file = App.get_file_store().get_file(
+      "journals/" + date.toDateString() + ".md"
+    );
     if (!file) {
-      file = App.get_file_store().create_file("journals/"+date.toDateString() + '.md');
+      file = App.get_file_store().create_file(
+        "journals/" + date.toDateString() + ".md"
+      );
     }
     markdown_editor.file = file;
+  };
+  const textarea = this.document.getElementById("scratchPadInput");
+  let file = App.get_file_store().get_file("scratch.txt");
+  if (file) {
+    textarea.value = file.get_content();
   }
-  const textarea = this.document.getElementById('scratchPadInput');
-  textarea.addEventListener('input', () => {
+  textarea.addEventListener("input", () => {
     const content = textarea.value;
     if (content) {
-      let file = App.get_file_store().get_file('scratch.txt');
+      let file = App.get_file_store().get_file("scratch.txt");
       if (!file) {
-        file = App.get_file_store().create_file('scratch.txt');
+        file = App.get_file_store().create_file("scratch.txt");
       }
-      //Todo: modify file's content
+      textarea.addEventListener("input", () => {
+        const content = textarea.value;
+        if (content) {
+          let file = App.get_file_store().get_file("scratch.txt");
+          if (!file) {
+            file = App.get_file_store().create_file("scratch.txt");
+          }
+          file.set_content(content);
+          App.get_file_store().sync();
+        }
+      });
     }
   });
 });
-
