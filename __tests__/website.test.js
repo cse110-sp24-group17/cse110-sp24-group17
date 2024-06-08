3cae99473923646d961672d3cfef29b00ffee02f
Melissa2002 <mosherof@ucsd.edu>
2024-06-08 01:24:30 -0700

    Swiping to Other view
    - Completed swiping functionality to see other view and using the scratch pad. The scratch pad actually doesn't work (in the sense that it doesn't update content) so case is failing, but if still doesn't work by tomorrow night the test can be deleted

--- __tests__/website.test.js
+++ __tests__/website.test.js
@@ -1,3 +1,5 @@
+import {setTimeout} from "node:timers/promises";
+
 describe('Basic user flow for Website', () => {
     beforeAll(async () => {
         await page.goto('https://cse110-sp24-group17.github.io/cse110-sp24-group17/source/main');
@@ -6,88 +8,78 @@ describe('Basic user flow for Website', () => {
     // Check to make sure that the file starts off with a root directory
     it('Initial File Explorer - it is empty ', async () => {
         console.log('Checking files...');
-        // Query select the shadow root of the file explorer
-        const fileExplorer = await page.$('file-explorer');
+        const fileExplorer = await page.$('file-explorer');  // Query select the shadow root of the file explorer
         const shadow = await fileExplorer.getProperty("shadowRoot");
-        // Get the root directory
-        const root = await shadow.$(".root");
+        const root = await shadow.$(".root"); // Get the root directory
 
-      // Check if the root exists
-      expect(root).not.toBeNull();
+      expect(root).not.toBeNull(); // Check if the root exists
     }, 50000);
 
     // Check to make sure that a file can be added
     it('Adding to File Explorer', async () => {
         console.log('Adding files...');
 
-        // Query the addFile button
-        const addFileButton = await page.$('#addFile');
+        const addFileButton = await page.$('#addFile'); // Query the addFile button
         await addFileButton.click();
 
-        // Get the shadow root of the file explorer
-        const fileExplorer = await page.$('file-explorer');
+        const fileExplorer = await page.$('file-explorer');  // Get the shadow root of the file explorer
         const shadow = await fileExplorer.getProperty("shadowRoot");
-
-        // Record children files and folders from the beginning
         const ogChildren = await shadow.$eval("#file", el => el.children);
 
-        // Submit the new file name 
-        const form = await shadow.$("#add-file");
+        const form = await shadow.$("#add-file"); // Submit the new file name 
         await form.click();
         await page.keyboard.type("ThisIsANewFile");
         const submit = await form.$('input[type=submit]')
         await submit.click();
 
-        // Check that one element was added
+        await addFileButton.click();
+         await form.click();
+         await page.keyboard.type("Another file");
+         await submit.click();
+
         const childrenFiles = await shadow.$eval("#file", el => el.children);
-        expect(Object.keys(childrenFiles).length).toBe(Object.keys(ogChildren).length + 1);
+        expect(Object.keys(childrenFiles).length).toBe(Object.keys(ogChildren).length + 2);
       }, 50000);
     
     // Check to make sure that a folder is added correctly
     it('Adding to Folder Explorer', async () => {
         console.log('Adding folder...');
 
-        // Query the addFolder button
         const addFolderButton = await page.$('#addFolder');
         await addFolderButton.click();
 
-        // Get the shadow root of the file explorer
         const fileExplorer = await page.$('file-explorer');
         const shadow = await fileExplorer.getProperty("shadowRoot");
-
-        // Record children files and folders from the beginning
         const ogChildren = await shadow.$eval("#file", el => el.children);
 
-        // Submit the new folder name 
-        const form = await shadow.$("#add-folder");
+        const form = await shadow.$("#add-folder"); // Submit the new folder name 
         await form.click();
         await page.keyboard.type("ThisIsANewFolder");
         const submit = await form.$('input[type=submit]')
         await submit.click();
 
-        // Check that one element was added
+        await addFolderButton.click();  // Submit another folder
+        await form.click();
+        await page.keyboard.type("ThisIsAnotherFolder");
+        await submit.click();
+
         const childrenFiles = await shadow.$eval("#file", el => el.children);
-        expect(Object.keys(childrenFiles).length).toBe(Object.keys(ogChildren).length + 1);
+        expect(Object.keys(childrenFiles).length).toBe(Object.keys(ogChildren).length + 2);
       }, 50000);
 
     // Check to make sure that a element in the directory can be deleted
     it('Deleting from File Explorer', async () => {
         console.log('Deleting an element from file explorer...');
 
-        // Get the shadow root of the file explorer
         const fileExplorer = await page.$('file-explorer');
         const shadow = await fileExplorer.getProperty("shadowRoot");
-
-        // Record children files and folders from the beginning
         let children = await shadow.$eval("#file", el=>el.children);
         const numChildren = Object.keys(children).length;
 
-        // Query the button to delete elements
-        const deleteButton = await page.$('#trashIcon');
+        const deleteButton = await page.$('#trashIcon'); // Query the button to delete elements
         await deleteButton.click();
 
-        // Get the first text file and delete it  
-        const childToDelete =await shadow.$(".file-entry");
+        const childToDelete =await shadow.$(".file-entry"); // Get the first text file and delete it  
         await childToDelete.click();
     
         let newChildren = await shadow.$eval("#file", el =>el.children);
@@ -95,31 +87,51 @@ describe('Basic user flow for Website', () => {
 
       }, 50000);
 
-    // FIXME
+    // Check to make sure that information in the file explorer stays on reload
+    it('Reloading the page', async () => {
+      console.log('Reloading the page...');
+
+      const fileExplorer = await page.$('file-explorer'); 
+      const shadow = await fileExplorer.getProperty("shadowRoot");
+      let children = await shadow.$eval("#file", el=>el.children);
+      const numChildren = Object.keys(children).length;
+
+      await page.reload();
+
+      const newfileExplorer = await page.$('file-explorer');
+      const newshadow = await newfileExplorer.getProperty("shadowRoot");
+      let newChildren = await newshadow.$eval("#file", el =>el.children);
+
+      expect(numChildren).toBe(Object.keys(newChildren).length);
+    }, 50000);
+
     // Switch from file explorer view to project view
     it('Switching from file explorer view to project view', async () => {
-        console.log('Switching view...');
+      console.log('Switching view...');
 
-        let moveObj = await page.$('#ProjectView');
-        const fileOffset = await page.$eval('#ProjectView', el => el.getAttribute("class"));
-        const journalOffset = await page.$eval('#JournalView', el => el.getAttribute("class"));
-        console.log(journalOffset);
-        console.log(fileOffset);
-       
-        if (fileOffset == "swiper-slide swiper-slide-next") {
-            moveObj = await page.$('#JournalView');
-        }
+      const addFileButton = await page.$('#addFile'); // Grab an element from the file explorer that can be dragged to flip to Journal View
+      let rect1 = await addFileButton.boundingBox();
+      await page.mouse.move(rect1.x, rect1.y);
+      await page.mouse.down();
+      await page.mouse.move(500,0, {delay: 20000});
+      await page.mouse.up();
+      await setTimeout(999);
 
-        let rect = await moveObj.boundingBox();
-        console.log(rect);
-        page.mouse.move(0,0);
-        await page.mouse.move(rect.x + rect.width / 2, rect.y + rect.height / 2);
-        await page.mouse.down();
-        await page.mouse.move(0, 0);
-        await page.mouse.up();
-        
-        const file = await page.$eval('#ProjectView', el => el.getAttribute("class"));
-        expect(file).toBe(journalOffset);
-      }, 50000); 
+      let journalStatus = await page.$eval("#JournalView", el => el.getAttribute("class"));
+      expect(journalStatus).toBe("swiper-slide swiper-slide-visible swiper-slide-active");
+    }, 50000); 
+
+    // Add a note to the journal notepad //CURRENTLY THIS FUNCTIONALITY HAS NOT BEEN ADDED - delete if it does not get completed
+    it('Switching from file explorer view to project view', async () => {
+      console.log('Switching view...');
+
+      const scratchPad = await page.$('#scratchPadInput');
+      await scratchPad.click()
+      await page.keyboard.type("I am writing a note that is very important!");
+      const newNote = await page.$eval("#scratchPadInput", el => el.innerHTML)
+      const calendar = await page.$("#calendar");
+      await calendar.click();
+      expect(newNote).toBe("I am writing a note that is very important!");
+    }, 50000); 
   
   });
